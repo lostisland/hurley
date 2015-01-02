ef365c6eb7e6e8b122bad0883fddb64b88e83043
Rick Olson <technoweenie@gmail.com>
2015-01-01 21:04:51 -0700

    more integration tests

--- test/client_test.rb
+++ test/client_test.rb
@@ -19,45 +19,5 @@ module Hurley
       url = req.url
       assert_equal "https://example.com/a/b?a=1", url.to_s
     end
-
-    def test_before_callback
-      c = Client.new "https://example.com"
-      c.connection = Test.new do |test|
-        test.post "/a" do |req|
-          assert_equal "BOOYA", req.body
-          [200, {}, "meh"]
-        end
-      end
-
-      c.before_call do |req|
-        req.body = req.body.to_s.upcase
-      end
-
-      res = c.post "a" do |req|
-        req.body = "booya"
-      end
-
-      assert_equal 200, res.status_code
-      assert c.connection.all_run?
-    end
-
-    def test_after_callback
-      c = Client.new "https://example.com"
-      c.connection = Test.new do |test|
-        test.get "/a" do |req|
-          [200, {}, "meh"]
-        end
-      end
-
-      c.after_call do |res|
-        res.body = res.body.to_s.upcase
-      end
-
-      res = c.get "a"
-
-      assert_equal 200, res.status_code
-      assert_equal "MEH", res.body
-      assert c.connection.all_run?
-    end
   end
 end
