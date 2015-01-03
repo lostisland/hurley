9a2022abca1d1adc010111509a09e20bcfa2dfbc
risk danger olson <technoweenie@gmail.com>
2015-01-03 16:44:25 -0700

    update multipart docs

--- lib/hurley/multipart.rb
+++ lib/hurley/multipart.rb
@@ -1,6 +1,62 @@
 require "securerandom"
 
+# Taken from multipart-post gem: https://github.com/nicksieger/multipart-post
+# Removes coupling with net/http
+
 module Hurley
+  # Convenience methods for dealing with files and IO that are to be uploaded.
+  class UploadIO
+    # Create an upload IO suitable for including in the body hash of a
+    # Hurley::Request.
+    #
+    # Can take two forms. The first accepts a filename and content type, and
+    # opens the file for reading (to be closed by finalizer).
+    #
+    # The second accepts an already-open IO, but also requires a third argument,
+    # the filename from which it was opened (particularly useful/recommended if
+    # uploading directly from a form in a framework, which often save the file to
+    # an arbitrarily named RackMultipart file in /tmp).
+    #
+    # Usage:
+    #
+    #     UploadIO.new("file.txt", "text/plain")
+    #     UploadIO.new(file_io, "text/plain", "file.txt")
+    #
+    attr_reader :content_type, :original_filename, :local_path, :io, :opts
+
+    def initialize(filename_or_io, content_type, filename = nil, opts = {})
+      io = filename_or_io
+      local_path = nil
+      if io.respond_to?(:read)
+        # in Ruby 1.9.2, StringIOs no longer respond to path
+        # (since they respond to :length, so we don't need their local path, see parts.rb:41)
+        local_path = filename_or_io.respond_to?(:path) ? filename_or_io.path : DEFAULT_LOCAL_PATH
+      else
+        io = File.open(filename_or_io)
+        local_path = filename_or_io
+      end
+
+      filename ||= local_path
+
+      @content_type = content_type
+      @original_filename = File.basename(filename)
+      @local_path = local_path
+      @io = io
+      @opts = opts
+    end
+
+    def method_missing(*args)
+      @io.send(*args)
+    end
+
+    def respond_to?(meth, include_all = false)
+      @io.respond_to?(meth, include_all) || super(meth, include_all)
+    end
+
+    DEFAULT_LOCAL_PATH = "local.path".freeze
+  end
+
+  # Internal helper classes for generating multipart bodies.
   module Multipart
     def self.boundary
       "Hurley-#{SecureRandom.hex}"
@@ -181,57 +237,5 @@ END
     end
 
     BINARY = "BINARY".freeze
-  end
-
-  # Convenience methods for dealing with files and IO that are to be uploaded.
-  class UploadIO
-    # Create an upload IO suitable for including in the params hash of a
-    # Net::HTTP::Post::Multipart.
-    #
-    # Can take two forms. The first accepts a filename and content type, and
-    # opens the file for reading (to be closed by finalizer).
-    #
-    # The second accepts an already-open IO, but also requires a third argument,
-    # the filename from which it was opened (particularly useful/recommended if
-    # uploading directly from a form in a framework, which often save the file to
-    # an arbitrarily named RackMultipart file in /tmp).
-    #
-    # Usage:
-    #
-    #     UploadIO.new("file.txt", "text/plain")
-    #     UploadIO.new(file_io, "text/plain", "file.txt")
-    #
-    attr_reader :content_type, :original_filename, :local_path, :io, :opts
-
-    def initialize(filename_or_io, content_type, filename = nil, opts = {})
-      io = filename_or_io
-      local_path = nil
-      if io.respond_to?(:read)
-        # in Ruby 1.9.2, StringIOs no longer respond to path
-        # (since they respond to :length, so we don't need their local path, see parts.rb:41)
-        local_path = filename_or_io.respond_to?(:path) ? filename_or_io.path : DEFAULT_LOCAL_PATH
-      else
-        io = File.open(filename_or_io)
-        local_path = filename_or_io
-      end
-
-      filename ||= local_path
-
-      @content_type = content_type
-      @original_filename = File.basename(filename)
-      @local_path = local_path
-      @io = io
-      @opts = opts
-    end
-
-    def method_missing(*args)
-      @io.send(*args)
-    end
-
-    def respond_to?(meth, include_all = false)
-      @io.respond_to?(meth, include_all) || super(meth, include_all)
-    end
-
-    DEFAULT_LOCAL_PATH = "local.path".freeze
   end
 end
