1ed123dc275da4cdfb63eeb624145eab80e87dca
Rick Olson <technoweenie@gmail.com>
2015-01-05 09:23:16 -0700

    wat

--- test/helper.rb
+++ test/helper.rb
@@ -2,7 +2,9 @@ require "minitest/autorun"
 require File.expand_path("../../lib/hurley", __FILE__)
 Hurley.require_lib "test"
 
+puts $LOAD_PATH
+
 module Hurley
-  class TestCase < MiniTest::Test
-  end
+  base_class = defined?(MiniTest::Test) ? MiniTest::Test : MiniTest::Unit::TestCase
+  TestCase = Class.new(base_class)
 end
